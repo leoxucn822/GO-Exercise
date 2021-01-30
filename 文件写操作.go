b3452556d36da44452fab75be9017fdfb8a0f9b0
leoxucn822 <leo.xucn822@gmail.com>
2021-01-30 18:17:58 +0800

    go practise

---
+++ 文件写操作.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	str, error := os.OpenFile("/tmp/test", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0644)
+	if error != nil {
+		log.Fatal("there is a issue while open the files!")
+	}
+	defer str.Close()
+	write := bufio.NewWriter(str)
+	write.WriteString("hello go programming language!\n")
+	write.Flush()
+}
