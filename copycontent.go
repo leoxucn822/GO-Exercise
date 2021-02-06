82c095437e5649b1304650f8796d4a701dcb8cee
leoxucn822 <leo.xucn822@gmail.com>
2021-02-06 11:08:11 +0800

    go practise

--- copycontent.go
+++ copycontent.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	filepath1 := "/etc/passwd"
+	filepath2 := "/tmp/passwd"
+
+	data, err := ioutil.ReadFile(filepath1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(filepath2, data, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
